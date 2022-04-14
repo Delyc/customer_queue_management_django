070f3e7523fa5a559414d384044980e647d02997
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-04-14 05:35:42 +0200

    adding footer

--- queue_management/settings.py
+++ queue_management/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from re import template
+import django_heroku
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7fq5b_n@_@7y8zdjw!%hx84o!)*0^ay3j!2kv1ep(&=!hofn0&'
+# SECRET_KEY = 'django-insecure-7fq5b_n@_@7y8zdjw!%hx84o!)*0^ay3j!2kv1ep(&=!hofn0&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,3 +131,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS=["https://customer-que.herokuapp.com"]
+django_heroku.settings(locals())
